7d0fb0e54d069a89516107f2b8af80632be2626a
Full Name <karin.isaev@gmail.com>
2019-03-25 18:00:30 -0400

    new

--- NewTCGA_2017scripts/Thesis_final_scripts/Prognostic_lncs_more_detail_paper/008BB_V3_LGG_vs_GBM_lncRNA_specific_pathways_166_cands_summarizing_coPCGs.R
+++ NewTCGA_2017scripts/Thesis_final_scripts/Prognostic_lncs_more_detail_paper/008BB_V3_LGG_vs_GBM_lncRNA_specific_pathways_166_cands_summarizing_coPCGs.R
@@ -1,80 +1,32 @@
 library(survAUC)
-#source("source_code_Cox_MonteCarlo_CV_Mar13.R")
 require(caTools)
 #check if this person is in my analysis: TCGA-61-2095
 library(glmnet)
 library(survcomp)
 library(caret)
 library(stringr)
-library(EnvStats)
-library(patchwork)
-
-source("universal_LASSO_survival_script.R")
-
 library(ggpubr)
 library(ggrepel)
 library(viridis)
 library(patchwork)
 library(caret)  
 library(Rtsne)
-library(data.table)
+library(EnvStats)
 library(reshape2)
 
-#------DATA---------------------------------------------------------
-#UCSC gene info
-ucsc <- fread("UCSC_hg19_gene_annotations_downlJuly27byKI.txt", data.table=F)
-#z <- which(ucsc$hg19.ensemblSource.source %in% c("antisense", "lincRNA", "protein_coding"))
-#ucsc <- ucsc[z,]
-z <- which(duplicated(ucsc[,8]))
-ucsc <- ucsc[-z,]
-
-#fantom 
-fantom <- fread("lncs_wENSGids.txt", data.table=F) #6088 lncRNAs 
-extract3 <- function(row){
-  gene <- as.character(row[[1]])
-  ens <- gsub("\\..*","",gene)
-  return(ens)
-}
-fantom[,1] <- apply(fantom[,1:2], 1, extract3)
-#remove duplicate gene names (gene names with multiple ensembl ids)
-z <- which(duplicated(fantom$CAT_geneName))
-rm <- fantom$CAT_geneName[z]
-z <- which(fantom$CAT_geneName %in% rm)
-fantom <- fantom[-z,]
-
-#save RNA and PCG files locally
-#saveRDS(rna, file="rna_lncRNAs_expression_data_june29.rds")
-#saveRDS(pcg, file="rna_pcg_expression_data_june29.rds")
-
-rna = readRDS("rna_lncRNAs_expression_data_june29.rds")
-pcg = readRDS("rna_pcg_expression_data_june29.rds")
-
-#Combined into one dataframe because need to get ranks 
-all <- merge(rna, pcg, by = c("patient", "Cancer"))
-all = all[,1:25170]
+source("check_lnc_exp_cancers.R")
 
 #------FEATURES-----------------------------------------------------
 
 allCands = readRDS("final_candidates_TCGA_PCAWG_results_100CVsofElasticNet_June15.rds")
-allCands = subset(allCands, data == "TCGA") #175 unique lncRNA-cancer combos, #166 unique lncRNAs 
+allCands = subset(allCands, data == "TCGA") #173 unique lncRNA-cancer combos, #166 unique lncRNAs 
 allCands$combo = unique(paste(allCands$gene, allCands$cancer, sep="_"))
 cands_dups = unique(allCands$gene[which(duplicated(allCands$gene))])
-
-val_cands = read.csv("175_lncRNA_cancers_combos_23_cancer_types_july5.csv")
-val_cands = as.data.table(val_cands)
-val_cands = subset(val_cands, data == "PCAWG") #175 unique lncRNA-cancer combos, #166 unique lncRNAs 
-val_cands$combo = unique(paste(val_cands$gene, val_cands$cancer, sep="_"))
-val_cands = subset(val_cands, top_pcawg_val == "YES") #175 unique lncRNA-cancer combos, #166 unique lncRNAs 
-val_cands = subset(val_cands, as.numeric(pval) < 0.05)
+colnames(allCands)[7] = "Cancer"
+allCands = merge(allCands, canc_conv, by="Cancer")
+allCands$combo = paste(allCands$gene, allCands$type)
 
 #------PCGs for pathway analysis--------------------------------------
-
-pcgs_risks_both = readRDS("pcgs_enriched_in_risk_groups_non_lncRNA_risk_groups_pcg_analysis_july24.rds")
-risk = as.data.table(filter(pcgs_risks_both, V2 == "Risk"))
-nonrisk = as.data.table(filter(pcgs_risks_both, V2 == "NonRisk"))
-
-all_de_results = readRDS("coexpression_results_processed_july24.rds")
-all_de_results = as.data.table(all_de_results)
 
 all_de_results = readRDS("diff_expressed_PCGs_lncRNA_risk_groups_Aug21.rds")
 all_de_results = as.data.table(all_de_results)
@@ -84,6 +36,9 @@ all_de_results = all_de_results[-z,]
 all_de_results_lgg = readRDS("diff_expressed_PCGs_lncRNA_risk_groups_lgg_nov30.rds")
 all_de_results_lgg = as.data.table(all_de_results_lgg)
 all_de_results = rbind(all_de_results, all_de_results_lgg)
+
+all_de_results$combo = paste(all_de_results$lnc, all_de_results$cancer) #156/162 unique combos across 21 unique cancer types 
+all_de_results = as.data.table(filter(all_de_results, combo %in% allCands$combo))
 
 #------make matrix of lncRNA candidates within each cancer type
 #and their associated PCGs
@@ -112,7 +67,7 @@ make_matrix_for_ap = function(canc){
   dat_all_matrix = acast(dat_all, ID~lnc, value.var="P.Value")
 	dat_all_matrix[is.na(dat_all_matrix)] = 1
 
-	file = paste("Aug22_DE_genes_fActivePathways/", canc, "all_up_down_genes_fActivepathways_Dec13_updFC.rds", sep="_")
+	file = paste("Aug22_DE_genes_fActivePathways/", canc, "all_up_down_genes_fActivepathways_March25_updFC.rds", sep="_")
 	saveRDS(dat_all_matrix, file)
 
 	lncs = unique(dat$lnc)
@@ -199,11 +154,25 @@ sig_paths_sum = unique(sig_paths_sum)
 sig_paths_sum = merge(sig_paths_sum, sig_des_sum, by="combo")
 sig_paths_sum$canc = sapply(sig_paths_sum$combo, function(x){unlist(strsplit(x, "_"))[2]})
 
+
 canc_conv = rna[,c("type", "Cancer")]
 canc_conv = unique(canc_conv)
 colnames(canc_conv)[1] = "canc"
 sig_paths_sum = merge(sig_paths_sum, canc_conv, by="canc")
 
+sig_des = as.data.table(filter(sig_des, combo %in% sig_paths_sum$combo))
+all_lnc_pathways_df = as.data.table(filter(all_lnc_pathways_df, combo %in% sig_paths_sum$combo))
+
+#COSMIC cancer gene census
+census = read.csv("Census_allFri_Jul_13_16_55_59_2018.csv")
+#get ensg
+get_census_ensg = function(genes){
+  glist = unlist(strsplit(genes, ","))
+  z = which(str_detect(glist, "ENSG"))
+  ensg = glist[z]
+  return(ensg)
+}
+census$ensg = sapply(census$Synonyms, get_census_ensg)
 
 pdf("summary_#DE_pcgs_vs_pathways_pathways_figure_jdan2519.pdf", width=9, height=5)
 g = ggscatter(sig_paths_sum, x = "num_sig_des", y = "num_sig_pathways",
@@ -307,11 +276,30 @@ all_res = as.data.table(filter(all_res, num_sig_des >=75))
 
 #117 unique lncRNA-cancer pairs 
 #114 unique lncRNAs 
+
+z = which(all_res$gene %in% census$lnc)
+census_cands = all_res[z,]
+census_cands$type = "census_genes"
+for(i in 1:length(census_cands$combo)){
+  genes = subset(census, combo %in% census_cands$combo[i])
+  num = dim(genes)[1]
+  census_cands$num_sig_des[i] = num
+}
+
+all_res = rbind(census_cands, all_res)
+all_res$combo3 = paste(all_res$combo, all_res$type)
+z = which(duplicated(all_res$combo3))
+if(!(length(z)==0)){
+all_res = all_res[-z,]}
+
+all_res$num_sig_des = log2(all_res$num_sig_des)
+all_res$type = factor(all_res$type, levels = c("genes", "census_genes", "pathways"))
+
 pdf("summary_barplot_genenames_#DE_pcgs_vs_pathways_pathways_figure_jan2519.pdf", width=12, height=4)
 g = ggplot(all_res, aes(combo2, num_sig_des, group=type)) + theme_classic() + 
-   geom_col(position = "dodge", aes(fill=type), color="grey29")+xlab("lncRNA-cancer pair") + ylab("Number of sig genes/pathways")+
+   geom_col(position = position_stack(), aes(fill=type), color="grey29")+xlab("lncRNA-cancer pair") + ylab("Number of sig genes/pathways")+
    theme(legend.title=element_blank(), legend.position="top", axis.title.x=element_blank(), 
-   	axis.text.x = element_text(angle = 65, hjust = 1, colour = a, size=6)) + scale_fill_manual(values=c("snow3", "#E69F00", "#56B4E9"))
+   	axis.text.x = element_text(angle = 65, hjust = 1, colour = a, size=6)) + scale_fill_manual(values=c("red", "#E69F00", "#56B4E9"))
 #ggpar(g,
 # font.tickslab = c(6,"plain", "black"),
 # xtickslab.rt = 45)
---
+++ NewTCGA_2017scripts/Thesis_final_scripts/Prognostic_lncs_more_detail_paper/008_all_but_not_lgg_AA_diff_Exp_pcgs_Aug21.R
@@ -0,0 +1,243 @@
+#008_AA_diff_Exp_pcgs_Aug21.R
+
+#------------------------------------------------------------------------------
+#Take each lncRNA candidate and conduct LIMMA diff exp between lncRNA
+#high and low risk groups
+#------------------------------------------------------------------------------
+
+library(edgeR)
+library(limma)
+#library(Glimma)
+#library(gplots)
+#library(org.Mm.eg.db)
+#library(RColorBrewer)
+
+#source code
+source("check_lnc_exp_cancers.R")
+library(corrplot)
+
+#COSMIC cancer gene census
+census = read.csv("Census_allFri_Jul_13_16_55_59_2018.csv")
+#get ensg
+get_census_ensg = function(genes){
+  glist = unlist(strsplit(genes, ","))
+  z = which(str_detect(glist, "ENSG"))
+  ensg = glist[z]
+  return(ensg)
+}
+census$ensg = sapply(census$Synonyms, get_census_ensg)
+
+#HiC data 
+load("hic_data.rsav")
+#remove rownames
+rownames(hic_data) = c(1:nrow(hic_data))
+
+pcg_counts = readRDS("counts_19438_lncRNAs_tcga_all_cancers_March13_wclinical_data.rds")
+
+#------FEATURES-----------------------------------------------------
+
+allCands = readRDS("final_candidates_TCGA_PCAWG_results_100CVsofElasticNet_June15.rds")
+allCands = subset(allCands, data == "TCGA") #175 unique lncRNA-cancer combos, #166 unique lncRNAs 
+allCands$combo = unique(paste(allCands$gene, allCands$cancer, sep="_"))
+
+#------------------------------------------------------------------
+#Within each tissue type, rank lncRNAs by which percentile of 
+#expression they fall into to then compare with PCAWG lncRNAS exp
+#------------------------------------------------------------------
+
+#2. Get lncRNA - median within each tissue type
+allCands$combo = paste(allCands$gene, allCands$cancer, sep="_")
+combos = unique(allCands$combo)
+
+#canc type to cancer conversion
+canc_conv = unique(rna[,c("type", "Cancer")])
+
+#3. Want ranking seperatley for high lncRNA expression group versus low lncRNA expression group
+#---------------------------------------------------------
+#Function 1
+#for each lnc-cancer, label patient as lncRNA-risk or non-risk 
+#---------------------------------------------------------
+
+get_lnc_canc = function(comb){
+	lnc = unlist(strsplit(comb, "_"))[1]
+	canc = unlist(strsplit(comb, "_"))[2]
+	canc_type = canc_conv$type[which(canc_conv$Cancer == canc)]
+	canc_data = subset(pcg_counts, type == canc_type)
+	
+	z = which(colnames(rna) %in% c("patient", lnc, "type"))
+	lnc_dat = rna[,z]
+	z = which(lnc_dat$type == canc_type)
+	lnc_dat = lnc_dat[z,]
+	med = median(as.numeric(lnc_dat[,2]))
+	if(med ==0){
+		z = which(lnc_dat[,2] > 0)
+		lnc_dat$lnc_tag = ""
+		lnc_dat$lnc_tag[z] = "high"
+		lnc_dat$lnc_tag[-z] = "low"
+	}
+	
+	if(!(med ==0)){
+		z = which(lnc_dat[,2] >= med)
+		lnc_dat$lnc_tag = ""
+		lnc_dat$lnc_tag[z] = "high"
+		lnc_dat$lnc_tag[-z] = "low"
+	}
+
+	#merge back with orgiinal dataframe
+	canc_data = merge(canc_data, lnc_dat, by=c("patient", "type"))
+
+	#keep only PCGs not lncRNAs 
+	pcgs_id = unique(colnames(pcg))
+	z = which(colnames(canc_data) %in% c(pcgs_id, colnames(lnc_dat)))
+	canc_data = canc_data[,z]
+
+	#Remove PCGs with median E < 5 FPKM 	
+	#get medians of all PCGs
+	z = which(str_detect(colnames(canc_data), "ENSG"))	
+
+  		#cox ph
+  		z = which(allCands$combo == comb)
+  		HR = as.numeric(allCands$HR[z])
+  		
+  		if(HR <1){
+  			risk = "low"
+  			canc_data$risk = ""
+  			canc_data$risk[canc_data$median=="high"] ="noRISK"
+  			canc_data$risk[canc_data$median=="low"] ="RISK"
+  		}
+  		if(HR >1){
+  			risk = "high"
+  			canc_data$risk = ""
+  			canc_data$risk[canc_data$median=="high"] ="RISK"
+  			canc_data$risk[canc_data$median=="low"] ="noRISK"
+  		}
+
+  		canc_data$lnc = lnc
+  		canc_data$canc = canc
+  		colnames(canc_data)[which(colnames(canc_data)==lnc)] = "lncRNA"
+
+  	return(canc_data)	
+	}#end function evaluate_each_lnc
+
+all_canc_lnc_data = llply(combos, get_lnc_canc, .progress="text")
+
+#---------------------------------------------------------
+#Function 2
+#wtihin each cancer 
+#calculate for each lncRNAs differentially expressed PCGs
+#---------------------------------------------------------
+
+diffE <- function(d){
+	
+	z = which(str_detect(colnames(d), "ENSG"))	
+	rownames(d) = d$patient
+
+	design <- model.matrix(~ 0 + factor(d$lnc_tag))
+	colnames(design) <- c("high", "low")
+	rownames(d) <- d$patient
+
+	expression <- t(d[,z])
+
+	# Obtain CPMs
+	myCPM <- cpm(expression)
+	# Have a look at the output
+	head(myCPM)
+
+	# Which values in myCPM are greater than 0.5?
+	thresh <- myCPM > 0.5
+	# This produces a logical matrix with TRUEs and FALSEs
+	head(thresh)
+	
+	# Summary of how many TRUEs there are in each row
+	table(rowSums(thresh))
+
+	# we would like to keep genes that have at least 10 TRUES in each row of thresh
+	keep <- rowSums(thresh) >= 10
+	# Subset the rows of countdata to keep the more highly expressed genes
+	myCPM = myCPM[keep,]	
+
+	counts.keep <- expression[keep,]
+	summary(keep)
+	dim(counts.keep)
+
+	y <- DGEList(counts.keep)
+	# have a look at y
+	#y
+
+	# Library size information is stored in the samples slot
+	y$samples
+	# Get log2 counts per million
+	logcounts <- cpm(y,log=TRUE)
+	
+	#TMM normalization to eliminate composition biases between libraries 
+	# Apply normalisation to DGEList object
+	y <- calcNormFactors(y)
+
+	#apply voom normalization 
+	v <- voom(y,design,plot = TRUE)
+
+	# Fit the linear model
+	fit <- lmFit(v)
+	names(fit)
+
+	cont.matrix <- makeContrasts(LowvsHigh=high-low, levels=design)
+	fit2 <- contrasts.fit(fit, cont.matrix)
+	fit2 <- eBayes(fit2)
+	ps <- fit2$p.value
+	ps <- p.adjust(ps, method="fdr")
+	#numGenes <- length(which(ps <= 0.05))
+    t <- topTable(fit2, coef=1, n="Inf")
+    t$ID = rownames(t)
+    t$cancer = d$type[1]
+    
+	#dist of fold change
+	summary(t$logFC)
+	t$gene_name = llply(t$ID, get_name_pcg)
+	t$lnc = d$lnc[1]
+	t$gene_name = as.character(t$gene_name) 
+    #plot fold changes
+    #p <- ggplot(t, aes(logFC, -log10(adj.P.Val)))
+	#print(p + geom_point(alpha = 0.55, color="lightcyan4") +
+	#geom_vline(xintercept=log(2), linetype="dashed", color = "red")+
+	#geom_vline(xintercept=log(0.5), linetype="dashed", color = "red")+
+	#geom_hline(yintercept=-log10(0.05), linetype="dashed", color = "gray40")+
+	#geom_text_repel(
+    #data = filter(t, -log10(adj.P.Val) > -log10(0.05) , abs(logFC) >= 2),
+    #aes(label=gene_name), size =2, 
+    #segment.size  = 0.2,
+    #segment.color = "grey50")+
+	#ggtitle(paste(d$Cancer[1], d$lnc[1], get_name(d$lnc[1]))))
+
+	#only include those with FC >2 or <2
+	t = as.data.table(filter(t, adj.P.Val <=0.05 & ((logFC <= log(0.5)) | (logFC >= log(2)))))
+
+    #if(dim(t)[1] <= 1){
+    #	t <- c(paste(d$lnc[1], d$canc[1]), "none")
+   	#}
+
+    return(t)   	
+}
+
+#pdf("LGG_HOXA10_volcano_plot_Aug21.pdf")
+#diffE(all_canc_lnc_data[[2]])
+#dev.off()
+
+#pdf("volcano_plots_diffE_lncRNA_risks.pdf")
+#diffEresults = llply(all_canc_lnc_data, diffE, .progress="text")
+diffEresults = llply(all_canc_lnc_data, diffE, .progress="text")
+#dev.off()
+
+diffEresults1 = ldply(diffEresults, data.frame)
+diffEresults1 = as.data.table(diffEresults1)
+saveRDS(diffEresults1, file="diff_expressed_PCGs_lncRNA_risk_groups_Aug21.rds")
+#saveRDS(diffEresults1, file="diff_expressed_PCGs_lncRNA_risk_groups_lgg_nov30.rds")
+colnames(allCands)[7] = "Cancer"
+allCands = merge(allCands, canc_conv, by="Cancer")
+allCands$combo = paste(allCands$gene, allCands$type)
+diffEresults1$combo = paste(diffEresults1$lnc, diffEresults1$cancer)
+diffEresults1 = as.data.table(filter(diffEresults1, combo %in% allCands$combo))
+
+##########
+###DONE###
+##########
+
