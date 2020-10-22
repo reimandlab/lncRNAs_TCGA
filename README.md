b09034a6b6a539a9271e80b6fa1aba8587194336
karini925 <karin.isaev@gmail.com>
2020-10-22 14:48:31 -0400

    update code

--- README.md
+++
@@ -1,1 +0,0 @@
-# Evaluating lncRNA expression in cancers 
--- pcawg_analysis_July2017/sourcing_scripts.R
+++
@@ -1,8 +0,0 @@
-##Sourcing scripts
-
-# source our functions
-code.dir <- "c:\temp"
-code.files = dir(code.dir, pattern = "[.r]")
-for (file in code.files){
-  source(file = file.path(code.dir,file))
-}
