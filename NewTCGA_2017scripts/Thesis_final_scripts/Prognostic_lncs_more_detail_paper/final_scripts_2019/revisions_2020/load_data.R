f2b03f6347ce815eb642e55e30bcff59e7dee013
karini925 <karin.isaev@gmail.com>
2020-11-29 18:56:57 -0500

    update load data

--- NewTCGA_2017scripts/Thesis_final_scripts/Prognostic_lncs_more_detail_paper/final_scripts_2019/revisions_2020/load_data.R
+++ NewTCGA_2017scripts/Thesis_final_scripts/Prognostic_lncs_more_detail_paper/final_scripts_2019/revisions_2020/load_data.R
@@ -21,15 +21,18 @@ rm <- fantom$CAT_geneName[z]
 z <- which(fantom$CAT_geneName %in% rm)
 fantom <- fantom[-z,]
 colnames(fantom)[1] = "gene"
+
 hg38 = readRDS("lncRNAs_2019_manuscript/grch38_dat_annotables.rds")
 hg38$chr = paste("chr", hg38$chr, sep="")
-z = which(fantom$gene %in% hg38$ensgene)
-fantom = fantom[z,]
+
+#z = which(fantom$gene %in% hg38$ensgene)
+#fantom = fantom[z,]
 hg38_lncs = as.data.table(filter(hg38, ensgene %in% fantom$gene, !(biotype == "protein_coding")))
 colnames(hg38_lncs)[1] = "gene"
-fantom=merge(fantom, hg38_lncs, by="gene")
-colnames(fantom)[2]="hg19_name"
-colnames(fantom)[8]="CAT_geneName"
+
+#fantom=merge(fantom, hg38_lncs, by="gene")
+#colnames(fantom)[2]="hg19_name"
+#colnames(fantom)[8]="CAT_geneName"
 
 ###---------------------------------------------------------------
 ###Get lncRNAs for each cancer
@@ -135,13 +138,13 @@ ucsc <- fread("UCSC_hg19_gene_annotations_downlJuly27byKI.txt", data.table=F)
 z <- which(duplicated(ucsc[,8]))
 ucsc <- ucsc[-z,]
 
-#manual_missing_lncs = as.data.frame(matrix(ncol=9, nrow=3))
-#manual_missing_lncs[1,]=c("ENSG00000247844", "na", "CCAT1", "chr8", 127207866, 127219088, "-1",  "lincRNA", "na")
-#manual_missing_lncs[2,]=c("ENSG00000233800", "na", "RP11-295G24.4", "chr20", 44448777, 44450092, "1",  "sense_intronic", "na")
-#manual_missing_lncs[3,]=c("ENSG00000237907", "na", "LINC01430", "chr8", 127207866, 127219088, "-1",  "antisense_RNA", "na")
-#colnames(manual_missing_lncs)=colnames(hg38)
+manual_missing_lncs = as.data.frame(matrix(ncol=9, nrow=3))
+manual_missing_lncs[1,]=c("ENSG00000247844", "na", "CCAT1", "chr8", 127207866, 127219088, "-1",  "lincRNA", "na")
+manual_missing_lncs[2,]=c("ENSG00000233800", "na", "RP11-295G24.4", "chr20", 44448777, 44450092, "1",  "sense_intronic", "na")
+manual_missing_lncs[3,]=c("ENSG00000237907", "na", "LINC01430", "chr8", 127207866, 127219088, "-1",  "antisense_RNA", "na")
+colnames(manual_missing_lncs)=colnames(hg38)
 
-#hg38 = rbind(hg38, manual_missing_lncs)
+hg38 = rbind(hg38, manual_missing_lncs)
 
 #remove cancer types with less than 50 patients
 pats_num = as.data.table(table(all$type))
