11979d5df12eba58e324d2a2d673a8348640c004
karini925 <karin.isaev@gmail.com>
2020-12-09 19:03:32 -0500

    update figure 2

--- NewTCGA_2017scripts/Thesis_final_scripts/Prognostic_lncs_more_detail_paper/final_scripts_2019/revisions_2020/Figure2A_code.R
+++ NewTCGA_2017scripts/Thesis_final_scripts/Prognostic_lncs_more_detail_paper/final_scripts_2019/revisions_2020/Figure2A_code.R
@@ -8,10 +8,12 @@ lapply(packages, require, character.only = TRUE)
 
 date = Sys.Date()
 
+setwd("Documents/lncRNAs")
+
 #figure 2
-res = readRDS("Documents/final_candidates_TCGA_PCAWG_results_100CVsofElasticNet_June15.rds")
+res = readRDS("final_candidates_TCGA_PCAWG_results_100CVsofElasticNet_June15.rds")
 res = as.data.table(filter(res, data=="TCGA"))
-canc_conv = readRDS("Documents/canc_conv.rds")
+canc_conv = readRDS("canc_conv.rds")
 colnames(canc_conv)[2] = "cancer"
 z=which(res$gene_name == "HOXA-AS4")
 res$gene_name[z] = "HOXA10-AS"
@@ -37,13 +39,21 @@ res$gene_name = factor(res$gene_name, levels=order)
 #res = res[1:20,]
 res$HR = as.numeric(res$HR)
 res$HR = log2(res$HR)
-res$fdr = -log10(res$fdr)
+res$fdr_pval = -log10(res$fdr_pval)
+res$low95 = as.numeric(res$low95)
+res$low95 = log2(res$low95)
+res$upper95 = as.numeric(res$upper95)
+res$upper95 = log2(res$upper95)
 
-pdf("lncRNA_candidates_final_figure2B.pdf", width=14, height=6)
+pdf("Dec2020/lncRNA_candidates_final_figure2B.pdf", width=14, height=6)
 
 g = ggplot(data=res, aes(x=gene_name, y=HR, order = -HR)) +
-  geom_bar(stat="identity", aes(fill=fdr),colour="black") + facet_grid(~ type, scale="free", space = "free")+
-  geom_hline(yintercept=0, linetype="dashed", color = "red") + theme_minimal()
+  geom_bar(stat="identity", aes(fill=fdr_pval),colour="black") +
+  geom_errorbar(aes(ymin=low95, ymax=upper95),
+                  width=.25,  size=.2,                   # Width of the error bars
+                  position=position_dodge(.9), color="darkred") +
+  facet_grid(~ type, scale="free", space = "free")+
+  geom_hline(yintercept=0, color = "black") + theme_bw()
 ggpar(g, xtickslab.rt=90, font.tickslab=c(6, "plain", "black"),
       legend = "bottom", legend.title = "Wald test, -log10(FDR)",
       font.legend = c(10, "plain", "black")) + scale_fill_gradient(low = "white", high = "black")+
